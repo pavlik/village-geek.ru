23522fbcca060e610893e547299912359900f593
Pavel Pavlenko <region23@gmail.com>
2016-01-04 11:12:31 +0300

    rebuilding site понедельник,  4 января 2016 г. 11:12:31 (MSK)

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t hugo-theme-blueberry-detox
+hugo server -t casper
 
 # Add changes to git.
 git add -A
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:pavlik/village-geek.ru.git gh-pages
+#git subtree push --prefix=public git@github.com:pavlik/village-geek.ru.git gh-pages
